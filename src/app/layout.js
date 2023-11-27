10d57f02ed01a1f708161019d6615c507b7f5cfa
Nazmul Hridoy <hridoyn75@gmail.com>
2023-11-27 17:12:36 +0600

    added toploader

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import NextTopLoader from 'nextjs-toploader'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+      <NextTopLoader color="#ff5724"  showSpinner={false}
+  initialPosition={0.1} />
+      {children}</body>
     </html>
   )
 }
